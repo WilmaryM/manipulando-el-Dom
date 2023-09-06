fa40ed9c0a125fd95d70f778e634f6c01932c14b
wilmary <wilmarymercedes1404@gmail.com>
2023-09-06 10:14:28 -0400

    clase02 completa

--- 1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,7 +1,12 @@
 
 const btn = document.querySelector('[data-form-btn]')
-console.log(btn);
 
-btn.addEventListener("click", function (){
-    console.log("crear tarea")
-}); 
+//arrow functions O funcion anonima
+const creatTask = (evento) => {
+    evento.preventDefault()
+    const input = document.querySelector('[data-form-input]')
+    console.log(input.value)
+}
+console.log(btn)
+
+btn.addEventListener("click", creatTask); 
